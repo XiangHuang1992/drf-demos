7b73a6608051c13d2a30135a820808d78d7a0789
huangxiang <huangxiang1992@hotmail.com>
2018-09-05 05:47:57 +0800

    创建drones模块的view，serializer，model，并初步调通

--- restful01/restful01/settings.py
+++ restful01/restful01/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     # Django Rest Framework
     'rest_framework',
     # Toys Application
-    'toys.apps.ToysConfig'
+    'toys.apps.ToysConfig',
+    # drones application
+    'drones.apps.DronesConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,13 +79,23 @@ WSGI_APPLICATION = 'restful01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+      'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'drones',
+           'USER': 'huangxiang',
+           'PASSWORD': 'hx15502040571.',
+           'HOST': '127.0.0.1',
+           'PORT': '5432'
+      }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
